e0fa85fa1c1bef780a3d40492cce20bede177e95
Dominic Quintero <dominicaq09@gmail.com>
2022-10-21 22:24:12 -0700

    Fixed healthbar UI

--- Assets/Scripts/UI/interfacePlayerHP.cs
+++ Assets/Scripts/UI/interfacePlayerHP.cs
@@ -6,7 +6,7 @@ public class interfacePlayerHP : MonoBehaviour
     public Transform player;
     private EntityHealth m_playerHp;
     public TextMeshProUGUI m_PlayerHpText;
-    //private int oldVal = int.MaxValue;
+    private int oldVal = int.MaxValue;
     
     void Start() 
     {
@@ -17,13 +17,11 @@ public class interfacePlayerHP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
         int val = (int)m_playerHp.currentHealth;
         if (val != oldVal)
         {
             oldVal = val;
             m_PlayerHpText.text = val.ToString();
         }
-        */
     }
 }
